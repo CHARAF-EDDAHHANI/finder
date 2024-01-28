a4f45913d92e6433e6b6b06556ddc4df65ff9294
Charaf eddahhani <125513039+HONORPRIDE@users.noreply.github.com>
2024-01-28 18:08:49 +0100

    Update filestorage.py

--- hr_console/engine/filestorage.py
+++ hr_console/engine/filestorage.py
@@ -6,9 +6,19 @@ class FileStorage:
         with open(filename, 'w') as file:
             data = [obj.to_dict() for obj in objects]
             json.dump(data, file, indent=2)
+        print(f"Data saved to {filename}")
 
     @staticmethod
     def load_from_json(class_type, filename):
-        with open(filename, 'r') as file:
-            data = json.load(file)
-            return [class_type.from_dict(obj) for obj in data]
+        try:
+            with open(filename, 'r') as file:
+                data = json.load(file)
+                objects = [class_type.from_dict(obj) for obj in data]
+            print(f"Data loaded from {filename}")
+            return objects
+        except FileNotFoundError:
+            print(f"File not found: {filename}")
+            return []
+        except Exception as e:
+            print(f"Error loading data from {filename}: {e}")
+            return []
